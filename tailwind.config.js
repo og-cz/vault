454e671eed7c2322439166404f4ca49a8c089f14
og-cz <16796127+og-cz@users.noreply.github.com>
2026-02-10 12:30:42 +0800

    initialize TypeScript and Vite frontend configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './main.tsx',
+    './App.tsx',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
